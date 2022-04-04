436f66699d49e13182b98c709e8db0ad3bae2ae5
Court Schuett <schuettc@amazon.com>
2022-04-03 23:48:44 -0500

    first commit

--- .projenrc.js
+++ .projenrc.js
@@ -1,12 +1,31 @@
 const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.1.0',
+  name: 'cdk-lex-import',
+  depsUpgradeOptions: {
+    ignoreProjen: false,
+    workflowOptions: {
+      labels: ['auto-approve', 'auto-merge'],
+    },
+  },
+  autoApproveOptions: {
+    secret: 'GITHUB_TOKEN',
+    allowedUsernames: ['schuettc'],
+  },
+  autoApproveUpgrades: true,
+  projenUpgradeSecret: 'PROJEN_GITHUB_TOKEN',
+  cdkVersion: '2.19.0',
+  license: 'MIT-0',
+  copyrightOwner: 'Court Schuett',
   defaultReleaseBranch: 'main',
-  name: 'cdk-lex-upload',
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
 });
+
+const common_exclude = [
+  'cdk.out',
+  'cdk.context.json',
+  'yarn-error.log',
+  'dependabot.yml',
+  '.DS_Store',
+];
+
+project.gitignore.exclude(...common_exclude);
 project.synth();
