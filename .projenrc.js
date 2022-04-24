74850d37849aadb847072eec2fb18d012dc6e791
Court Schuett <schuettc@amazon.com>
2022-04-24 15:43:00 -0500

    fix: update desc

--- .projenrc.js
+++ .projenrc.js
@@ -5,6 +5,8 @@ const project = new awscdk.AwsCdkConstructLibrary({
   cdkVersion: '2.20.0',
   defaultReleaseBranch: 'main',
   name: 'cdk-lex-zip-import',
+  description: 'cdk construct for importing a zipped Lex bot',
+  keywords: ['cdk', 'lex', 'import', 'bot'],
   repositoryUrl: 'https://github.com/schuettc/cdk-lex-zip-import.git',
   eslintOptions: {
     ignorePatterns: ['example/**'],
